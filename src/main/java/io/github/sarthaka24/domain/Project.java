a304660c21444413ff003f23f890bab0e8b42ea8
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-16 16:28:15 +0530

    added fields, constructor, getter and setter and toString for Project domain

--- src/main/java/io/github/sarthaka24/domain/Project.java
+++ src/main/java/io/github/sarthaka24/domain/Project.java
@@ -1,5 +1,38 @@
 package io.github.sarthaka24.domain;
 
 public class Project {
+    private int id;
+    private String name;
 
+    public Project() {
+    }
+
+    public Project(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
